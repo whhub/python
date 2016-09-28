4a004a7a3ee9c976749b60afd4c4837dbf80b8dc
whhub <wanghui122004@me.com>
2016-09-28 21:43:03 +0800

    ext5 print

---
+++ LearnPythonTheHardWay/ex5_print.py
@@ -0,0 +1,21 @@
+name = 'woofie'
+age = 32 # not a lie
+height = 171 # cm
+weight = 62 # kg
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black'
+
+print("Let's talk about %s." % name)
+print("He's %d cm tall." % height)
+print("He's %d kg heavy." % weight)
+print("Actually that's not too light.")
+print("He's got %s eyes and %s hair." % (eyes, hair))
+print("His teeth are usually %s depending on the coffee." % teeth)
+
+# this line is tricky, try to get it exactly right
+print("If I add %d, %d, and %d , I get %d." % (age, height, weight, age + height + weight))
+
+# additional exercise
+# -*- coding: utf-8 -*-
+# print("age = %r, heigt = %d inches, weight = %d pounds." % (age)
