eeddfadb12235ca81d74bce47209753c8d161379
whhub <wanghui122004@me.com>
2016-10-08 09:26:52 +0800

    ex15 ReadFile

---
+++ LearnPythonTheHardWay/ex16_ReadWriteFile.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+script, filename = argv
+
+print("We're going to erase %r." % filename)
+print("If you don't want that, hit CTRL-C (^C).")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
