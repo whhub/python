2f7dcd60cd94f3efdfd9c78d7b84b3c7f91c430b
whhub <wanghui122004@me.com>
2016-10-13 20:53:19 +0800

    ex41 super

---
+++ LearnPythonTheHardWay/ex41_moreClass.py
@@ -0,0 +1,43 @@
+# Animal is-a object 9yes, sort of confusing) look at the extra credit
+class Animal(object):
+    pass
+
+
+class Dog(Animal):
+    def __init__(self, name):
+        self.name = name
+
+
+class Cat(Animal):
+    def __init__(self, name):
+        self.name = name
+
+
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+        set.pet = None
+
+
+class Employee(Person):
+    def __init__(self, name, salary):
+        super(Employee, self).__init__(name)
+        self.salary = salary
+
+
+class Fish(object):
+    pass
+
+
+class Salmon(Fish):
+    pass
+
+
+class Halibut(Fish):
+    pass
+
+
+rover = Dog("Rover")
+satan = Cat("Satan")
+mary = Person("Mary")
+
